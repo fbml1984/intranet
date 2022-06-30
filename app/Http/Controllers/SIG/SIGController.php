8026f93a02873d06f7d73bb5e907fda837c02449
Fábio <fsbrancaglioni@gmail.com>
2022-06-30 19:16:56 -0300

    Testando visualização de documento sem redirecionar

--- app/Http/Controllers/SIG/SIGController.php
+++ app/Http/Controllers/SIG/SIGController.php
@@ -22,8 +22,35 @@ class SIGController extends Controller
     public function view($id)
     {
         try {
-            $chave = SIG::requisitar();
-            return redirect()->to(env('API_URL') . "/controle_documentos/sgd?id={$id}&hash={$chave}");
+
+            $chave   = SIG::obterChave();
+            $url     = env('API_URL') . "/controle_documentos/sgd_v2?id={$id}&hash={$chave}";
+            $arquivo = file_get_contents($url);
+
+            if (!empty($arquivo)) {
+
+                $_arquivo = SIG::obterArquivoPorId($id);
+                if (empty($_arquivo)) {
+                    //throw
+                }
+
+                switch ($_arquivo->extensao) {
+                    case "pdf":
+                        header("Content-type: application/pdf");
+                        break;
+                    case "doc":
+                    case "docx":
+                        header('Content-Type: application/octet-stream');
+                        break;
+                    case "xls":
+                    case "xlsx":
+                        header('Content-Type: application/vnd.ms-excel');
+                        break;
+                }
+                header("Content-Disposition: inline; filename=\"{$_arquivo->arquivo}\"");
+                echo file_get_contents("data://application/{$_arquivo->extensao};base64," . $arquivo);
+                die;
+            }
         } catch (Exception $e) {
             dd($e);
         }
--- app/Models/SIG.php
+++ app/Models/SIG.php
@@ -23,11 +23,30 @@ class SIG extends Model
         return executeQuery($query);
     }
 
-    public static function requisitar()
+    public static function obterArquivoPorId($id)
+    {
+        $query = "
+            SELECT
+                *
+            FROM
+                controle_de_documentos.dbo.arquivos_mapeados
+            WHERE
+                id_arquivo = {$id}
+        ";
+        return collect(executeQuery($query))->first();
+    }
+
+    public static function obterChave()
     {
         $chave = md5(microtime());
         $query = "INSERT INTO CORPORERM_CLOUD.CI30A5_128116_RM_PD.DBO.SGO_CONTROLE_ACESSO_DOCUMENTOS(CHAVE) VALUES ('{$chave}')";
         executeQuery($query);
         return $chave;
+    }
+
+    public static function utilizar($chave)
+    {
+        $query = "DELETE FROM CORPORERM_CLOUD.CI30A5_128116_RM_PD.DBO.SGO_CONTROLE_ACESSO_DOCUMENTOS where chave='{$chave}'";
+        executeQuery($query);
     }
 }
